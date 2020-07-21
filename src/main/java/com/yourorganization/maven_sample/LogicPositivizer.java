ea288a816f2909f5a3daf0f93ca8bcf82bec1565
sanjeevikumaran <67994355+sanjeevikumaran@users.noreply.github.com>
2020-07-21 14:54:27 +0530

    Update LogicPositivizer.java

--- src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -32,7 +32,6 @@ public class LogicPositivizer {
         Log.info("Positivizing!");
          Log.info("Sanjeevi Testing Auto Maven Build!");
           Log.info("Testing the sample maven build");
-           Log.info(" Sanjeevi ");
         
         
         cu.accept(new ModifierVisitor<Void>() {
